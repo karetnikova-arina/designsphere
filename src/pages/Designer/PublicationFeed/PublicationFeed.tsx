a48b02937f9d41415882f0e2a618080c4887303e
karetnikova-arina <106513867+karetnikova-arina@users.noreply.github.com>
2024-06-11 00:03:10 +0300

    bugs4

--- src/pages/Designer/PublicationFeed/PublicationFeed.tsx
+++ src/pages/Designer/PublicationFeed/PublicationFeed.tsx
@@ -4,12 +4,9 @@ import {CardMain} from "../../../components/Cards/CardMain/CardMain.tsx";
 import {MAIN_PUBLICATION, MAIN_PUBLICATION_INTERFACE} from "../../../data/1main.ts";
 import {ProjectInAccount} from "../../../components/ProjectInAccount/ProjectInAccount.tsx";
 import {useEffect, useState} from "react";
-import {useSelector} from "react-redux";
-import {RootState} from "../../../store/store.ts";
 
 export function PublicationFeed() {
     const [windowCard, setWindowCard] = useState(false)
-    const {values} = useSelector((s: RootState) => s.form)
     const [filter, setFilter] = useState("")
     const [data, setData] = useState<MAIN_PUBLICATION_INTERFACE[]>([])
     useEffect(() => {
